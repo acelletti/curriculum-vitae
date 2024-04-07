f459eb8bab2f172ebe5b616f5f13a3c25883cee4
Andrea Celletti <celletti.andrea87@gmail.com>
2024-04-07 12:49:35 +0100

    chore: Update SEO tags

--- app.vue
+++ app.vue
@@ -7,11 +7,19 @@
 <script setup lang="ts">
 useSeoMeta({
   title: "Curriculum Vitae",
-  ogTitle: "Andrea Celletti's Curriculum Vitae",
   description:
     "A motivated, adaptable and responsible Software Engineer with strong experience as a Team Leader and a proven track record for innovation and critical thinking.",
+
+  // OG tags
+  ogUrl: "https://acelletti.github.io/curriculum-vitae/",
+  ogTitle: "Andrea Celletti's Curriculum Vitae",
   ogDescription: "Staff Software Engineer at Vonage.",
-  ogImage: "/curriculum-vitae/img/ogCard.png",
+  ogImage: "https://acelletti.github.io/curriculum-vitae/img/ogCard.png",
+
+  // twitter tags
   twitterCard: "summary_large_image",
+  twitterTitle: "Andrea Celletti's Curriculum Vitae",
+  twitterDescription: "Staff Software Engineer at Vonage.",
+  twitterImage: "https://acelletti.github.io/curriculum-vitae/img/ogCard.png",
 });
 </script>
