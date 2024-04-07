c3b4417e20e7c9962d52aae99c345b55bf408374
Andrea Celletti <celletti.andrea87@gmail.com>
2024-04-07 12:19:02 +0100

    feat(seo): Add SEO meta tags

--- app.vue
+++ app.vue
@@ -3,3 +3,15 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: "Curriculum Vitae",
+  ogTitle: "Andrea Celletti's Curriculum Vitae",
+  description:
+    "A motivated, adaptable and responsible Software Engineer with strong experience as a Team Leader and a proven track record for innovation and critical thinking.",
+  ogDescription: "Staff Software Engineer at Vonage.",
+  ogImage: "/curriculum-vitae/img/ogCard.png",
+  twitterCard: "summary_large_image",
+});
+</script>
