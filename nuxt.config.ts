5a9b67d6679d315b3ab92a6808ed7351a0ff3db9
Andrea Celletti <celletti.andrea87@gmail.com>
2024-04-07 12:32:40 +0100

    feat(analytics): Add google tag script boilerplate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,16 @@ export default defineNuxtConfig({
           href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
         },
       ],
+      script: [
+        {
+          src: "https://www.googletagmanager.com/gtag/js?id=G-RZLTNSQNC5",
+          async: true,
+        },
+        {
+          innerHTML:
+            "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-RZLTNSQNC5');",
+        },
+      ],
     },
   },
 });
